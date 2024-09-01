31924981365cccab41f23d5e9d7173f4f696e1e8
mlkad <dusmagambetovaaa@gmail.com>
2024-09-01 22:48:08 +0500

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        let isOpening = false;
+
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (char === bracketsConfig[j][0]) {
+                if (char === bracketsConfig[j][1] && stack[stack.length - 1] === char) {
+                    stack.pop(); 
+                } else {
+                    stack.push(char); 
+                }
+                isOpening = true;
+                break;
+            }
+        }
+
+        if (!isOpening) {
+            if (stack.length === 0) {
+                return false; 
+            }
+            let lastOpening = stack.pop();
+            for (let j = 0; j < bracketsConfig.length; j++) {
+                if (lastOpening === bracketsConfig[j][0] && char !== bracketsConfig[j][1]) {
+                    return false;
+                }
+            }
+        }
+    }
+
+    return stack.length === 0; 
 }
